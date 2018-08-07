2186bd0d908177783bb9e18d4d4d133c0e485d5b
Kalarrs Topham <k-dawg-klakr@hotmail.com>
2018-08-06 19:23:52 -0700

    Adding template project.

---
+++ aws/template/build.cmd
@@ -0,0 +1,4 @@
+cd src/
+dotnet restore
+dotnet lambda package --configuration release --framework netcoreapp2.1 --output-package bin/release/netcoreapp2.1/deploy-package.zip
+cd -
---
+++ aws/template/local/LocalEntryPoint.cs
@@ -0,0 +1,30 @@
+﻿using Kalarrs.Serverless.NetCore.Util;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace template.Local
+{
+    public static class LocalEntryPoint
+    {
+        public static void Main(string[] args)
+        {
+            Startup<Handler>.ServerlessProject = new ServerlessProject();
+            BuildWebHost(args).Run();
+        }
+
+        private static IWebHost BuildWebHost(string[] args)
+        {
+            return WebHost.CreateDefaultBuilder(args)
+                .ConfigureLogging((webhostContext, builder) =>
+                {
+                    builder.AddConfiguration(webhostContext.Configuration.GetSection("Logging"))
+                        .AddConsole()
+                        .AddDebug();
+                })
+                .UseStartup<Startup<Handler>>()
+                .UseUrls($"http://localhost:{Startup<Handler>.ServerlessProject.Port}")
+                .Build();
+        }
+    }
+}
